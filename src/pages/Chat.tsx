16eded2eaab2bca05d3909b47e04cb2d305bcd56
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2025-01-29 15:52:50 +0530

    FIX: integrate messages api in UI

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -15,30 +15,30 @@ const Chat = () => {
     };
     async function fetchMessages() {
         const messages = await getMessages("chat_5749044c-fde8-4ed8-b3b3-c30aff07757d");
-        console.log(messages);
+        setChatMessages(messages);
+        return messages;
     }
 
-    const { data, isPending } = useQuery({
+    const { isPending } = useQuery({
         queryKey: ["messages"],
         queryFn: fetchMessages,
     });
 
-    if(data){
-        setChatMessages(data);
-    }
 
     if (isPending) return <div>Loading...</div>;
     return (
         <div className="flex flex-col h-screen">
-            <div className="flex-1 flex flex-col space-y-4 overflow-y-auto pb-4">
-                <div className="flex flex-col items-end pr-6">
-                    <p className="bg-[#303030] rounded-full p-2.5 max-w-[30%] w-fit">User prompt</p>
-                </div>
-                <div className="flex flex-row justify-start pl-6 gap-5">
-                    <LuBrainCircuit className="text-4xl bg-[#303030] max-w-[30%] p-2 rounded-full" />
-                    <p className="max-w-[50%] w-fit">AI response Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam dolore debitis iusto soluta odit amet neque iste laborum esse dicta, voluptate, fuga accusantium et quam cupiditate nesciunt eligendi unde molestias!</p>
-                </div>
-            </div>
+            {chatMessages.map((message) => (
+                 <div key={message.messageId} className="flex-1 flex flex-col space-y-4 overflow-y-auto pb-4">
+                 <div className="flex flex-col items-end pr-6">
+                     <p className="bg-[#303030] rounded-full p-2.5 max-w-[30%] w-fit">{message.prompt}</p>
+                 </div>
+                 <div className="flex flex-row justify-start pl-6 gap-5">
+                     <LuBrainCircuit className="text-4xl bg-[#303030] max-w-[30%] p-2 rounded-full" />
+                     <p className="max-w-[50%] w-fit">{message.response}</p>
+                 </div>
+             </div>
+            ))}
             <div className="sticky flex pb-20 justify-center">
                 <InputComponent handleSubmit={handleSubmit} />
             </div>
