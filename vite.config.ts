2f146d5f6c68a83ce52bef276639d201c1a6fa27
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2025-02-01 10:56:08 +0530

    FIX: vercel.json and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+});
